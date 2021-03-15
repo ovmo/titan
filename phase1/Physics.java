0d0503681cb1cbc006ec1c204ad5098acc8060c1
Oscar Moeller von <ovm@OvMBPro.fritz.box>
2021-03-15 18:59:07 +0100

    test files not to worry about

---
+++ phase1/Physics.java
@@ -0,0 +1,13 @@
+public class Physics implements FunctionInterface 
+{
+	public Vector3dInterface call(double t, Vector3dInterface s)
+	{
+		
+	}
+
+
+
+
+
+	
+}
---
+++ phase1/dumpster.java
@@ -0,0 +1,66 @@
+ // Oscar Physics
+
+	// public int hitOrMiss () 
+	// private static String[] solarObjects;
+	// private static int masses;
+	// private static int velocity;
+	// private static int radius;
+	// static
+	// {
+	// solarObjects = {"Sun", "Mercury", "Venus", "Earth", "Luna", "Mars", "Jupiter", "Saturn","Titan", "Uranus", "Neptune", "SpaceCraft"};
+	// 	masses = {1989100000000000000000000000000, }
+
+	// 	radius ={4370005600}
+	// }
+
+// Pscar PlanetStart
+		// LinkedList<Planet> planets = new LinkedList<Planet>;
+		// if( args.length < 1 ){
+		// 	throw new RuntimeException("Please give the starting Solar State.")
+
+		// }
+		// else
+		// {
+		// 	String inputfile = args[0];
+
+		// 	boolean seen[] = null;
+
+
+		// 	try{
+		// 		FileReader fr = new FileReader(inputfile);
+		// 		BufferedReader br = new BufferedReader(fr);
+
+		// 		String record = new String();
+
+  //               //! THe first few lines of the file are allowed to be comments, staring with a // symbol.
+  //               //! These comments are only allowed at the top of the file.
+
+  //               //! -----------------------------------------
+		// 		while ((record = br.readLine()) != null){
+		// 			if( record.startsWith("//") ) continue;
+  //                   break; // Saw a line that did not start with a comment -- time to start reading the data in!
+  //               }	
+  //               String[] records = record.split(" ");
+  //               String name = records[0].substring(0, str.length() - 1);
+  //               planet = new Planet()
+  //               for (int i = 0; )
+
+  //           }
+  //           catch (IOException ex){
+  //               // catch possible io errors from readLine()
+  //           	System.out.println("Error! Problem reading file "+inputfile);
+  //           	System.exit(0);
+  //           }
+
+// Leo Visual
+	/*// //each radius is represented in metres.
+	// final double rSUN=695700000.00;
+	// final double rMERCURY=2440000.00;
+	// final double rVENUS=6051000.84;
+	// final double rEARTH=6378000.137;
+	// final double rMOON=1737000.53;
+	// final double rMARS=3389000.92;
+	// final double rJUPITER=69911000.00;
+	// final double rSATURN=58232000.00;
+	// final double rTITAN=2575000.5;
+	// }*/
