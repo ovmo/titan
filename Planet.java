16c35bff146ba71b0857aaf99448e6c43185f7af
Oscar Moeller <43478862+ovmo@users.noreply.github.com>
2021-03-15 16:05:35 +0100

    Add files via upload

---
+++ Planet.java
@@ -0,0 +1,23 @@
+public class Planet 
+{
+	public Planet (String namePlanet)
+	{
+		name = namePlanet;
+	}
+
+
+	public String name;
+	public int step;
+	
+	public double mass;
+	public double radius;
+	public double gravity;
+
+	public double velocityX;
+	public double velocityY;
+	public double velocityZ;
+
+	public double positionX;
+	public double positionY;
+	public double positionZ;
+}
