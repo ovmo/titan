ac9fb8172faabb45d006aa921aff63c7d283c1ab
Lorik1999 <72268460+Lorik1999@users.noreply.github.com>
2021-03-18 22:44:40 +0100

    Update SpaceCraft.java

--- src/PhysicsEngine/SpaceCraft.java
+++ src/PhysicsEngine/SpaceCraft.java
@@ -1,9 +1,9 @@
 package PhysicsEngine;
 
 public class SpaceCraft {
-
+    // initial constant vairbales, where the velocity v of the spacecraft must be: 48 km/s <= v <= 60 km/s
     private final double MAX_TAKEOFF = 60000;
-    private final double MIN_TAKEOFF = 11.186;
+    private final double MIN_TAKEOFF = 11.186; // MIN_TAKEOFF represents the minimum required escape velocity from Earth at a distance of 80km from the Earth's surface.
     private final double MIN_REACH = 48000;
     private double mass;            //Mass of the spacecraft
     private double takeoffV;        //Velocity of the spacecraft during the takeoff
