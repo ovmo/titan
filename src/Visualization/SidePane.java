2b1181e78bfe0522df9be1af2cb85fa67417b397
Chiara <72265489+chiarapaglioni@users.noreply.github.com>
2021-03-19 09:08:59 +0100

    Update SidePane.java

--- src/Visualization/SidePane.java
+++ src/Visualization/SidePane.java
@@ -1,7 +1,5 @@
 package Visualization;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
