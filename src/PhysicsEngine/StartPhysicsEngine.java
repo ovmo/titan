892f0b6ccf5c50e891f273a1f201b09e0b5335d6
Chiara <72265489+chiarapaglioni@users.noreply.github.com>
2021-03-19 09:37:31 +0100

    Update StartPhysicsEngine.java

--- src/PhysicsEngine/StartPhysicsEngine.java
+++ src/PhysicsEngine/StartPhysicsEngine.java
@@ -11,22 +11,13 @@ public class StartPhysicsEngine
 
 		PlanetStart start = new PlanetStart();
 
-		//testing
-		//Simple testing = new Simple ();
-		//testing.calc(8, 60000);
-
-		//launching
+		//Launch spacecraft
 		Simulator launch = new Simulator();
 
 		if(launch.launch(8, 57000)){
 
-//			System.out.println(Planet.planets[1].orbit.size());
-//			System.out.println(Planet.planets[1].orbit.get(1));
-//			System.out.println(Planet.planets[1].getOrbitX()[1]);
-
-			//starting visual simulation
+			//Start visual simulation
 			StartVisuals.start();
 		};
-
 	}
 }
