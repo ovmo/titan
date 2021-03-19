76ebfa8a3de8edf4acf08f4bdc9dc980789d6040
Chiara <72265489+chiarapaglioni@users.noreply.github.com>
2021-03-19 08:43:59 +0100

    Update SidePane.java

--- src/Visualization/SidePane.java
+++ src/Visualization/SidePane.java
@@ -39,31 +39,6 @@ public class SidePane
     }
 
     /**
-        Add a new button to the pane
-        @param x x-coordinate of the button on the pane
-        @param y y-coordinate of the button on the pane
-     */
-    public void addButton(double x, double y)
-    {
-        Button button = new Button();
-
-        Image imageButton = new Image(getClass().getResourceAsStream("/Visualization/Resources/Button.png"));
-        ImageView imageViewButton = new ImageView(imageButton);
-
-        imageViewButton.setFitHeight(80);
-        imageViewButton.setPreserveRatio(true);
-        //location of the button
-        button.setTranslateX(x);
-        button.setTranslateY(y);
-        //Size of the button
-        button.setPrefSize(80, 80);
-        button.setStyle("-fx-background-color: gray;");
-        button.setGraphic(imageViewButton);
-
-        sidePaneGroup.getChildren().add(button);
-    }
-
-    /**
      * Add a new label to the pane
      * @param s name of the label
      * @param x x-coordinate of the label
@@ -91,7 +66,6 @@ public class SidePane
     */
     public Group getPane()
     {
-        addButton(100, 40);
         addLabel("Date: ", 40, 200, Color.RED);
         addLabel("01-04-2020", 40, 225, Color.BLACK);
         addLabel("Distance from Earth: ", 40, 400, Color.RED);
