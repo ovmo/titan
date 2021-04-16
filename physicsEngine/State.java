ee8704c7cef5531e8445bb3ad58928d195558660
Chiara <72265489+chiarapaglioni@users.noreply.github.com>
2021-04-16 12:31:58 +0200

    Update State.java

--- physicsEngine/State.java
+++ physicsEngine/State.java
@@ -1,19 +1,46 @@
 import titan.RateInterface;
 import titan.StateInterface;
+import titan.Vector3dInterface;
 
-public class State implements StateInterface {
+public class State implements StateInterface{
 
-    // 2x2 array (9 planets (10 with probe), 2 properties - vel, pos)
+    Vector3dInterface[][] matrix;      //2x10 array (9 planets (10 with probe), 2 properties - vel, pos)
 
-    @Override
+    //Constructor
+    public State() {
+    }
+
+    //Initialize the first state
+    public void initializeState() {
+        matrix = new Vector3d[10][2];
+        PlanetStart p = new PlanetStart();
+
+        for (int i = 0; i < 10; i++) {
+            matrix[i][0] = new Vector3d(p.planets[i].velocityX, p.planets[i].velocityY, p.planets[i].velocityZ);    //Initialize positions
+            matrix[i][1] = new Vector3d(p.planets[i].positionX, p.planets[i].positionY, p.planets[i].positionZ);    //Initialize velocities
+        }
+    }
+
+    //Create new state after the update
+    // new state newState
+    // for (i in state) - loop through planets
+    // update vel: state[i][1] = state[i][1].addMul(step, rate[i]
+    // update pos: state[i][2] = state[i][2].addMul(step, state[i][1]
     public StateInterface addMul(double step, RateInterface rate) {
+        StateInterface newState = new State();
 
-        //new state newState
-        // for (i in state) - loop through planets
-        // update vel: state[i][1] = state[i][1].addMul(step, rate[i]
-        // update pos: state[i][2] = state[i][2].addMul(step, state[i][1]
-        // return newState
+        for (int i = 0; i < 10; i++) {
+            matrix[i][0] = matrix[i][0].addMul(step, rate.get(i));      //Update vel
+            matrix[i][1] = matrix[i][1].addMul(step, matrix[i][0]);     //Update pos
+        }
+        return newState;
+    }
 
-        return null;
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < 10; i++) {
+            str += "Vel: " + matrix[i][0].toString() + " Pos: " + matrix[i][1].toString();
+        }
+        return str;
     }
 }
