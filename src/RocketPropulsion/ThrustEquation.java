a12149854137228915473b97269c55a04d8cdca1
Lorik1999 <https://github.com/ovmo/titan.git>
2021-05-02 20:49:22 +0200

    Create ThrustEquation.java

---
+++ src/RocketPropulsion/ThrustEquation.java
@@ -0,0 +1,28 @@
+
+package RocketPropulsion;
+import PhysicsEngine.PlanetStart;
+import PhysicsEngine.SpaceCraft;
+
+import java.lang.Math;
+
+public class ThrustEquation {
+    // must calculate
+    private double  massFlowRate;
+    private double pressure;
+    private double velocity;
+    //Assuming the probe itself is a sphere
+    private final double AREA= (4*Math.PI* Math.pow(SpaceCraft.radius,2));
+
+    private double machNumber= 50000/320;
+
+
+
+
+
+
+
+
+
+}
+
+
