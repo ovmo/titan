341f280b99f4ac4d42236900e370a8db56a6be2b
Lorik1999 <https://github.com/ovmo/titan.git>
2021-05-03 22:09:34 +0200

    Update ThrustEquation.java

--- src/RocketPropulsion/ThrustEquation.java
+++ src/RocketPropulsion/ThrustEquation.java
@@ -6,14 +6,36 @@ import PhysicsEngine.SpaceCraft;
 import java.lang.Math;
 
 public class ThrustEquation {
-    // must calculate
-    private double  massFlowRate;
+
+    //find the value of pressure
     private double pressure;
     private double velocity;
     //Assuming the probe itself is a sphere
     private final double AREA= (4*Math.PI* Math.pow(SpaceCraft.radius,2));
 
+    //Initial takeoffV
     private double machNumber= 50000/320;
+
+
+    public static double calcMassFlowRate( ){
+        double massFlowRate;
+        //must find temperature variable first
+        double temperature;
+        double calcATimesP= AREA * pressure;
+        double calcTempDenom= Math.sqrt(temperature);
+
+    }
+
+    public static double calcSpecificHeatRatio( ){
+        final double R= 8.1345;
+        final double n= 6.02* Math.pow(10, 23);
+
+
+
+
+
+    }
+
 
 
 
